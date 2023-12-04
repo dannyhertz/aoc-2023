e2a050e313edc07168f3c9c15680a95d0a526367
Danny Hertz <me@dannyhertz.com>
2023-12-04 00:40:48 -0500

    Day 3 partially complete

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 mod day1;
 mod day2;
+mod day3;
 
 use std::fs;
 
 fn main() {
     // Read input data from a file or any other source
-    let input = fs::read_to_string("input/day2.txt").expect("Error reading input file");
-
+    let input = fs::read_to_string("input/day3.txt").expect("Error reading input file");
 
     // Print the result
-    println!("Day 2 Solution Part I: {}", day2::solve(&input));
-    println!("Day 2 Solution Part II: {}", day2::solve_2(&input));
+    // println!("Day 2 Solution Part I: {}", day2::solve(&input));
+    // println!("Day 2 Solution Part II: {}", day2::solve_2(&input));
+    println!("Day 3 Solution Part I: {}", day3::solve(&input));
 }
